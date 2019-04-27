86be2ad73399453f8edea2a0ae2781115b86a431
yicheng <yicheng.fzu@gmail.com>
2019-04-27 22:23:17 +0800

    Add default route and using swift 4.2

--- PacketTunnel/PacketTunnelProvider.swift
+++ PacketTunnel/PacketTunnelProvider.swift
@@ -17,7 +17,28 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         let networkSettings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "8.8.8.8")
         networkSettings.mtu = 1500
         let ipv4Settings = NEIPv4Settings(addresses: ["192.169.89.1"], subnetMasks: ["255.255.255.0"])
-        networkSettings.iPv4Settings = ipv4Settings
+        ipv4Settings.includedRoutes = [NEIPv4Route.default()]
+        ipv4Settings.excludedRoutes = [
+            NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
+            NEIPv4Route(destinationAddress: "100.64.0.0", subnetMask: "255.192.0.0"),
+            NEIPv4Route(destinationAddress: "127.0.0.0", subnetMask: "255.0.0.0"),
+            NEIPv4Route(destinationAddress: "169.254.0.0", subnetMask: "255.255.0.0"),
+            NEIPv4Route(destinationAddress: "172.16.0.0", subnetMask: "255.240.0.0"),
+            NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
+            NEIPv4Route(destinationAddress: "17.0.0.0", subnetMask: "255.0.0.0"),
+        ]
+        networkSettings.ipv4Settings = ipv4Settings
+        let proxySettings = NEProxySettings()
+        proxySettings.httpEnabled = true
+        proxySettings.httpServer = NEProxyServer(address: "127.0.0.1", port: 7890)
+        proxySettings.httpsEnabled = true
+        proxySettings.httpsServer = NEProxyServer(address: "127.0.0.1", port: 7890)
+        proxySettings.excludeSimpleHostnames = true
+        // This will match all domains
+        proxySettings.matchDomains = [""]
+        proxySettings.exceptionList = ["api.smoot.apple.com","configuration.apple.com","xp.apple.com","smp-device-content.apple.com","guzzoni.apple.com","captive.apple.com","*.ess.apple.com","*.push.apple.com","*.push-apple.com.akadns.net"]
+        networkSettings.proxySettings = proxySettings
+                
         TunnelInterface.setup(with: self.packetFlow)
         
         setTunnelNetworkSettings(networkSettings) {
